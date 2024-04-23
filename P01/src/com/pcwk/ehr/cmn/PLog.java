6ffa054b28a69ab94ed699771abfcdaf4b885969
이성연 <167048567+Lukelee88@users.noreply.github.com>
2024-04-23 16:03:50 +0900

    Add files via upload

---
+++ P01/src/com/pcwk/ehr/cmn/PLog.java
@@ -0,0 +1,7 @@
+package com.pcwk.ehr.cmn;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+public interface PLog {
+	static final Logger LOG = LogManager.getLogger();
+
+}
---
+++ P01/src/com/pcwk/ehr/pj01/VitalInfo.java
@@ -0,0 +1,60 @@
+package com.pcwk.ehr.pj01;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class VitalInfo {
+	private int BPM;// 심박수
+	private int SBP;// 수축기 혈압
+	private int DBP;// 이완기 혈압
+	private int bloodSugar;//혈당
+	private String checkTime;
+	
+	
+	
+	public String getCheckTime() {
+		return checkTime;
+	}
+
+	public void setCheckTime(String checkTime) {
+		this.checkTime = checkTime;
+	}
+
+	public VitalInfo(int bpm, int sbp, int dbp, int bloodSugar) {
+		super();
+		this.BPM = bpm;
+		this.SBP = sbp;
+		this.DBP = dbp;
+		this.bloodSugar = bloodSugar;
+		Date now = new Date();
+		SimpleDateFormat formatter =new SimpleDateFormat("yyyy-MM-dd hh:mm:ss a");
+		this.checkTime =formatter.format(now);
+	}
+	
+	public int getBpm() {
+		return BPM;
+	}
+	public void setBpm(int bpm) {
+		this.BPM = bpm;
+	}
+	public int getSbp() {
+		return SBP;
+	}
+	public void setSbp(int sbp) {
+		this.SBP = sbp;
+	}
+	public int getDbp() {
+		return DBP;
+	}
+	public void setDbp(int dbp) {
+		this.DBP = dbp;
+	}
+	public int getBloodSugar() {
+		return bloodSugar;
+	}
+	public void setBloodSugar(int bloodSugar) {
+		this.bloodSugar = bloodSugar;
+	}
+	
+
+}
