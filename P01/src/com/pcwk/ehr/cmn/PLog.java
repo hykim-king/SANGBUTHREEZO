069da2b03a13a81871f61b219349dc1ec39684ec
이성연 <167048567+Lukelee88@users.noreply.github.com>
2024-04-24 09:16:14 +0900

    Delete P01 directory

--- P01/src/com/pcwk/ehr/cmn/PLog.java
+++
@@ -1,7 +0,0 @@
-package com.pcwk.ehr.cmn;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-public interface PLog {
-	static final Logger LOG = LogManager.getLogger();
-
-}
