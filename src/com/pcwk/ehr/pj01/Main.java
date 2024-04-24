d432faf5316547a3a7b6e57135d97f2fc8583d00
S.Y.Lee <dltjddus88@gmail.com>
2024-04-24 17:04:13 +0900

    Test data added

--- src/com/pcwk/ehr/pj01/Main.java
+++ src/com/pcwk/ehr/pj01/Main.java
@@ -1,37 +1,41 @@
 package com.pcwk.ehr.pj01;
 
-import java.util.Scanner;
-
 import com.pcwk.ehr.cmn.PLog;
 
+public class Main implements PLog {
 
-
-public class Main implements PLog{
-	
-	
 	public static void main(String[] args) {
-		
-		
+
 		HospitalManagement<Patient> hospital = new HospitalManagement<Patient>("myHospital");
 		hospital.initHospital();
-		System.out.println(hospital.hospitalName+" 환자 관리를 시작합니다.");
+		System.out.println(hospital.hospitalName + " 환자 관리를 시작합니다.");
 		LoopThread loop = new LoopThread(hospital);
-		//VitalThread th =new VitalThread(hospital);
-		
+		// VitalThread th =new VitalThread(hospital);
 
-
-		//테스트용코드
-		Patient patient01=new Patient("이성연", "남성", "O", "19881003", 183, 100);
-		Patient patient02=new Patient("김성연", "여성", "A", "19881003", 163, 60);
-		VitalInfo vitalinfo01= new VitalInfo(80, 80, 120, 120);
-		VitalInfo vitalinfo02= new VitalInfo(60, 100, 180, 220);
+		// 테스트용코드
+		Patient patient01 = new Patient("이성연", "남성", "O", "19881003", 183, 100);
+		Patient patient02 = new Patient("김성연", "여성", "A", "19881003", 163, 60);
+		Patient patient03 = new Patient("박성연", "여성", "B", "19881003", 163, 60);
+		Patient patient04 = new Patient("장성연", "여성", "B", "19881003", 163, 60);
+		VitalInfo vitalinfo01 = new VitalInfo(80, 80, 120, 120);
+		VitalInfo vitalinfo02 = new VitalInfo(60, 100, 180, 220);
+		VitalInfo vitalinfo03 = new VitalInfo(90, 150, 230, 120);
+		VitalInfo vitalinfo04 = new VitalInfo(90, 60, 180, 320);
 		hospital.patients.add(patient01);
 		hospital.patients.add(patient02);
+		hospital.patients.add(patient03);
+		hospital.patients.add(patient04);
 		hospital.patients.get(0).vitalinfo.add(vitalinfo01);
 		hospital.patients.get(1).vitalinfo.add(vitalinfo02);
-		
-		
-		//th.start();
+		hospital.patients.get(2).vitalinfo.add(vitalinfo03);
+		hospital.patients.get(3).vitalinfo.add(vitalinfo04);
+		hospital.patients.get(0).status = PatientStatus.Safe;
+		hospital.patients.get(1).status = PatientStatus.Dangerous;
+		hospital.patients.get(2).status = PatientStatus.Safe;
+		hospital.patients.get(3).status = PatientStatus.Very_Dangerous;
+		hospital.patients.get(3).setNotified(false);
+
+		// th.start();
 		loop.start();
 		try {
 			loop.join();
@@ -39,12 +43,10 @@ public class Main implements PLog{
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		
+
 		LOG.debug("프로그램 종료");
 		hospital.saveWholeFile();
 
-		
 	}
 
 }
